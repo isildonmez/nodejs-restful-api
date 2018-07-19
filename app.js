f5717b612641adcade410c1f08b5e271066edb83
Isil Donmez <isil.donmez@gmail.com>
2018-07-19 13:46:18 +0100

    Add db.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var db = require('./db');
 
 module.exports = app;
 
---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds243041.mlab.com:43041/my_dear_db');
