35e4f56fb67bcc2b0d463c66e00a20a1c0a9439e
Isil Donmez <isil.donmez@gmail.com>
2018-07-20 12:26:17 +0100

    Add Get request by Id

--- user/UserController.js
+++ user/UserController.js
@@ -28,4 +28,14 @@ router.get('/', function (req, res) {
 
 });
 
+router.get('/:id', function (req, res) {
+
+  User.findById(req.params.id, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+    res.status(200).send(user);
+  });
+
+});
+
 module.exports = router;
